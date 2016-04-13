5e2f670c9d1d186481be671df3b1327da2e7b58d
LarryFielder <larryfielder@gmail.com>
2016-04-13 08:38:27 +0100

    Update ReadStatusesOfImportJobs.ps1

--- ReadStatusesOfImportJobs.ps1
+++ ReadStatusesOfImportJobs.ps1
@@ -1,19 +1,10 @@
-<# Disclaimer
- Microsoft provides programming examples for illustration only, without warranty either expressed or
- implied, including, but not limited to, the implied warranties of merchantability and/or fitness 
- for a particular purpose. 
- 
- This sample assumes that you are familiar with the programming language being demonstrated and the 
- tools used to create and debug procedures. Microsoft support professionals can help explain the 
- functionality of a particular procedure, but they will not modify these examples to provide added 
- functionality or construct procedures to meet your specific needs. if you have limited programming 
- experience, you may want to contact a Microsoft Certified Partner or the Microsoft fee-based consulting 
- line at (800) 936-5200. 
+<# 
 
 # User profile bulk API - Read all import jobs from the tenant # 
 # 13th of Apr, 2016 - Release v1.0
 # Author(s)
 Nano Nano
+
 # Notes
 Support for this script requires that you have installed update SharePoint Online SDK/CSOM redistributable
 with minimum version of 4622.1208 to the computer where the script is executed. 
